dc792d301b345034e1d239a1eea8301543eace3a
Aadil <aadilhabibsattar@gmail.com>
2025-07-28 17:26:37 +0600

    add project files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Battleship Game
+
+A web-based Battleship game built with HTML, CSS, and JavaScript.
+
+## Features
+
+-   Test-Driven Development using Jest
+-   Webpack for bundling
+-   Modular game architecture
+
+## Setup
+
+```bash
+npm install
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Battleship  ",
+            template: "./dist/index.html",
+        }),
+    ],
+};
